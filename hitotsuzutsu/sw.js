02452ae1e7e7c9745f7c8d925057cf800d9ba9aa
Ginji001 <38126022+Ginji001@users.noreply.github.com>
2026-09-22 13:00:05 +0900

    chore: refresh PWA cache for add button fix

--- hitotsuzutsu/sw.js
+++ hitotsuzutsu/sw.js
@@ -1,5 +1,5 @@
-const CACHE="hitotsuzutsu-v2";
-const ASSETS=["./","./index.html","./app.css","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="hitotsuzutsu-v3";
+const ASSETS=["./","./index.html","./app.css?v=3","./app.js?v=3","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
